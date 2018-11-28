43f27c8e8a32a273f7e793a995fa6a65d5a9beda
seddonm1 <seddonm1@gmail.com>
2018-11-29 08:27:12 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='ArcExporter',
       version='0.0.1',
@@ -6,7 +6,8 @@ setup(name='ArcExporter',
       author='Mike Seddon',
       author_email='',
       license='MIT',
-      packages=['arcexport'],
+      packages=find_packages(),
+      package_data={'templates': ['*']},
       entry_points={
           'nbconvert.exporters': [
               'arcexport = arcexport:ArcExporter'
