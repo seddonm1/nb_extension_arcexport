1ae8f11c8f35e2b09227361d7bd271ca07266713
seddonm1 <seddonm1@gmail.com>
2018-11-29 08:22:37 +1100

    Update __init__.py

--- arcexport/__init__.py
+++ arcexport/__init__.py
@@ -32,10 +32,12 @@ class ArcExporter(HTMLExporter):
         We want to inherit from HTML template, and have template under
         `./templates/` so append it to the search path. (see next section)
         """
+        print(super().template_path +
+              [os.path.join(os.path.dirname(__file__), "templates")])
         return super().template_path+[os.path.join(os.path.dirname(__file__), "templates")]
 
     def _template_file_default(self):
         """
         We want to use the new template we ship with our library.
         """
-        return 'test_template.tpl'  # full
+        return 'test_template'  # full
